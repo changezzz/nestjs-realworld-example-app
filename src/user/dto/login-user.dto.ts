188f4ef70e770d6fd1bc3c9329115e3d26e9f40c
andrew.deng <kobe73er@gmail.com>
2023-04-24 09:01:32 +0800

    api format

--- src/user/dto/login-user.dto.ts
+++ src/user/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class LoginUserDto {
 
+  @ApiProperty({type: String})
   @IsNotEmpty()
   readonly email: string;
 
+  @ApiProperty({type: String})
   @IsNotEmpty()
   readonly password: string;
 }
